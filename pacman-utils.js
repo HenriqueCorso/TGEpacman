56d67aa418e7082902b7c205c79e310030dca9b3
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-07-25 17:16:04 -0300

    Add files via upload

--- pacman-utils.js
+++ pacman-utils.js
@@ -6,14 +6,12 @@ const { map } = await TileMap.LoadFromFile({ url: './level1.hjson' });
 
 // Utility function to check if a tile is free for Pacman to move
 export const isTileFree = (pos, offset, tileSize) => {
-  // If the player is moving down or right, add the width and height of the player
-  // to the coordinates, which is the same as tileSize, to verify a hit right or below.
+  // If the player is moving down or right, add the width and height of the player to the coordinates, which is the same as tileSize, to verify a hit right or below.
   if (offset.x == 1 || offset.y == 1) {
     const mapPos = Vec2.Add(Vec2.ToInt(Vec2.DivScalar(pos, tileSize)), offset);
     return map.tileAt(mapPos.x, mapPos.y) === 0 || map.tileAt(mapPos.x, mapPos.y) === 2;
   } else {
-    // Otherwise, deduct the pixels of movement direction from the current position
-    // to verify a hit above or left.
+    // Otherwise, deduct the pixels of movement direction from the current position to verify a hit above or left.
     const mapPos = Vec2.ToInt(Vec2.DivScalar(Vec2.Add(pos, offset), tileSize));
     return map.tileAt(mapPos.x, mapPos.y) === 0 || map.tileAt(mapPos.x, mapPos.y) === 2;
   }

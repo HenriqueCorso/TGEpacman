f70ab3ef09d7b6da966ded579f615a562d8d4874
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-08-04 21:52:38 -0300

    Add files via upload

--- pacman-utils.js
+++ pacman-utils.js
@@ -5,11 +5,11 @@ export function isTileFree(pos, offset, tileSize) {
   // If the player is moving down or right, add the width and height of the player to the coordinates, which is the same as tileSize, to verify a hit right or below.
   if (offset.x == 1 || offset.y == 1) {
     const mapPos = Vec2.Add(Vec2.ToInt(Vec2.DivScalar(pos, tileSize)), offset);
-    return this.tileAt(mapPos.x, mapPos.y) === 0 || this.tileAt(mapPos.x, mapPos.y) === 2;
+    return this.tileAt(mapPos.x, mapPos.y) === 0 || this.tileAt(mapPos.x, mapPos.y) === 2 || this.tileAt(mapPos.x, mapPos.y) === 3;;
   } else {
     // Otherwise, deduct the pixels of movement direction from the current position to verify a hit above or left.
     const mapPos = Vec2.ToInt(Vec2.DivScalar(Vec2.Add(pos, offset), tileSize));
-    return this.tileAt(mapPos.x, mapPos.y) === 0 || this.tileAt(mapPos.x, mapPos.y) === 2;
+    return this.tileAt(mapPos.x, mapPos.y) === 0 || this.tileAt(mapPos.x, mapPos.y) === 2 || this.tileAt(mapPos.x, mapPos.y) === 3;
   }
 }
 
